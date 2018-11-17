f921e1df5687f347e43290c33eca8d1e23c9a14a
Nathan Peck <nathan.peck@okstate.edu>
2018-11-17 13:31:06 -0700

    JS  Pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+
+// Input function ()
+    // search form
+        // input field for searching artist names- store input as variable
+        // submit button - click event
+            // input validation with modal - if/else
+
+// make ajax calls
+    // bands in town - push object data into arrays for later for loop
+    // spotify -push object data into widget
+
+// else    //  Output function ()
+    // clear previous data
+    // for loop {}  to create output table for results from bands in town
+        // filter down by date
+        // include link to purchase tickets
+
+    // push artist id into spotify widget
+
+
